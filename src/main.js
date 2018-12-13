82bde8eaf3cc198bd79c4499f1f123548b377595
wpx <wangpx@minstone.com.cn>
2018-12-13 14:47:48 +0800

    集成基于vue的移动端UI框架

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './vuex/store'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+// 引入 cube-ui
+import Cube from 'cube-ui'
 // 引用 normalize.css
 import 'normalize.css'
 // 引入axios
@@ -13,8 +13,8 @@ import axios from 'axios'
 // 配合lib-flexible 实现移动端自适应布局
 import 'lib-flexible'
 
-// 注入Element
-Vue.use(ElementUI)
+// 注入 cube-ui
+Vue.use(Cube)
 // 注入axios
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
